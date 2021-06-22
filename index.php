99491e7ebcff6ec7aa0276b1f212a3dddd77df76
BARIŞ KURT <kurt-bar07@hotmail.com>
2021-06-22 18:57:48 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Barış Yazılım </title>
+    <meta http-equiv="refresh" content="10;URL=index.php">   
+        <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="load">
+  <pre class="term">baris@barisyazilim:~$ </pre>
+</div>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'>
+	</script>
+    <script src="js/js.js"></script>
+  </body>
+</html>
